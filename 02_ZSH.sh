bffeb5643d75d4f5706863a9bb564c6cdf1e4504
void <yorick@ctrlalt.dev>
2019-08-13 15:52:57 -0700

    Add oh my zsh and zshrc

--- 02_ZSH.sh
+++ 02_ZSH.sh
@@ -3,3 +3,5 @@
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- 06_CustomizeZSH.sh
+++ 06_CustomizeZSH.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env sh
 
-curl -fsSL "https://gist.githubusercontent.com/ctrlaltdev/1e0f7a0e4f683804f4d14df310581e8b/raw/0c4667f9757336bf2c9ecc52feb61791cd50539d/.aliases" -o ~/.aliases
-echo "if [ -f ~/.aliases ]; then
-    . ~/.aliases
-fi" >> ~/.zshrc
+curl -fsSL "https://raw.githubusercontent.com/ctrlaltdev/.env/master/.aliases" -o ~/.aliases
 
-echo "EDITOR='nvim'" >> ~/.zshrc
+brew install fortune
 
-echo "setopt PROMPT_SUBST
-PROMPT='%F{cyan}%n %F{magenta}~ %f'
-RPROMPT='%F{yellow}%~%f'" >> ~/.zshrc
-
-echo "eval $(thefuck --alias)" >> ~/.zshrc
+mv ~/.zshrc ~/.zshrc.oh.my.zsh
+curl -fsSL "https://raw.githubusercontent.com/ctrlaltdev/.env/master/.zshrc" -o ~/.zshrc
 
 source ~/.zshrc
