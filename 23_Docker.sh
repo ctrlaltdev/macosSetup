5ec17a4d6e8394becf42535290620033dada967d
Yorick Demichelis <yorick@techholding.co>
2022-05-26 17:15:15 -0700

    feat: switch to podman

--- 23_Docker.sh
+++ 23_Docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-open "https://hub.docker.com/editions/community/docker-ce-desktop-mac"
+brew install podman
+# open "https://hub.docker.com/editions/community/docker-ce-desktop-mac"
